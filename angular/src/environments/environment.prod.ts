2b0a29144a9242e4505c5ee4245af8a7afb3eeee
deryanurcaman <camanderya@gmail.com>
2022-06-05 13:01:58 +0300

    firebase project change

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
   firebase: {
-    projectId: 'health-app-ee23d',
-    appId: '1:713297027048:web:843aaa6c8d8cdea39b4e5d',
-    storageBucket: 'health-app-ee23d.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyDL21ouHNeTZyKtuP3VHT235qqVePbhJiA',
-    authDomain: 'health-app-ee23d.firebaseapp.com',
-    messagingSenderId: '713297027048',
+    apiKey: "AIzaSyCi-6vK9EMktfo2-kUOOhq54j81GRjcj98",
+    authDomain: "health-application-5e38e.firebaseapp.com",
+    projectId: "health-application-5e38e",
+    storageBucket: "health-application-5e38e.appspot.com",
+    messagingSenderId: "664699308727",
+    appId: "1:664699308727:web:4e1d2a73da24067a615b32"
   },
   production: true,
 };
