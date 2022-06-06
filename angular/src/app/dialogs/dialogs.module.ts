5b0e6d7ac3d4de4c022c70e771ce4e46a416283d
sinanaltinayak <sinanaltinayak@gmail.com>
2022-06-07 01:06:14 +0300

    -dialogs for patient information and monitoring were added

--- angular/src/app/dialogs/dialogs.module.ts
+++ angular/src/app/dialogs/dialogs.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule} from '@angular/forms';
 
+import { DiagnosisDialogComponent } from './diagnosis-dialog/diagnosis-dialog.component';
+import { PatientInformationDialogComponent } from './patient-information-dialog/patient-information-dialog.component';
+import { PatientMonitoringDialogComponent } from './patient-monitoring-dialog/patient-monitoring-dialog.component';
+
 import { MatDividerModule } from '@angular/material/divider';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatGridListModule } from '@angular/material/grid-list';
-
-import { DiagnosisDialogComponent } from './diagnosis-dialog/diagnosis-dialog.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -21,9 +30,16 @@ import { DiagnosisDialogComponent } from './diagnosis-dialog/diagnosis-dialog.co
     MatDialogModule,
     MatFormFieldModule,
     MatProgressSpinnerModule,
-    MatGridListModule
+    MatGridListModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCheckboxModule,
+    MatTableModule,
+    MatChipsModule,
+    MatIconModule
 
   ],
-  declarations: [DiagnosisDialogComponent]
+  declarations: [DiagnosisDialogComponent, PatientInformationDialogComponent, PatientMonitoringDialogComponent]
 })
 export class DialogsModule { }
